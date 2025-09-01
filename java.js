15e001f46d27586b1e19e4a3982422cc4c23ada6
Yami4286 <abdulhanan8974@gmail.com>
2025-09-02 01:19:52 +0500

    I made it almost straight 11 hours with breaks 3 hours , i am done , i didn't commit much i will put comments in code later to be understandable Were finally free from foundations

--- java.js
+++ java.js
@@ -1,28 +1,53 @@
 let first = "";
-let operand = "";
+let operand = 0;
 let second = "";
+let result = 0;
 
 const Scre = document.querySelector(".screen");
 const btn = document.querySelectorAll(".num");
  const maxChar = 10;
 btn.forEach(Ele => {Ele.addEventListener("click", () => { const getValue = Ele.getAttribute("value");
- if(!first == "" && !operand == ""){
-    Equal(first,second,operand);
- }
-    if(getValue === "="){
-        
-        const Ans = Equal(first, second, operand);
+     if(Scre.textContent === ""){
+        clearState();
+     }
+    if(getValue === "="){ 
+        const Ans = Equal();
      return;
     }
+  if(getValue === "E"){clearState(); return;}
 
-    if(getValue === "+" || getValue === "-" || getValue === "/" || getValue === "x"){OperaPress(getValue); }
+
+    if(getValue === "+" || getValue === "-" || getValue === "/" || getValue === "*"){OperaPress(getValue); }
+    
+ if(getValue === "."){
+   
+     if(first === "" && Scre.textContent.includes(".")){
+         
+         return;
+     }
+       if(second.includes(".")){
+       return;
+     }
+      
+     if( first.includes(".") && operand === ""){
+    
+        return;
+     }
+         
+    }
+ 
+
     if(getValue === "C"){
-        Scre.textContent = Scre.textContent.slice(0, -1); 
+         Scre.textContent = Scre.textContent.slice(0, -1); 
     return;}
-    if(first === ""){
+    if(first === "" || operand === ""){
             Scre.textContent += getValue;
-    }else {
-        second = Scre.textContent += getValue;
+            console.log(second);
+    }else{
+        console.log(second);
+         
+         second += getValue;
+         Scre.textContent += getValue;
     }
 
     let ans = Scre.textContent;
@@ -35,36 +60,56 @@ btn.forEach(Ele => {Ele.addEventListener("click", () => { const getValue = Ele.g
     
  }); });
 
- function check(op){
-    if(op === "+") {  return "+";}
-    if(op === "-"){ return "-";}
-    if(op === "x"){  return "x";}
-    if(op === "/"){ return "/";}
- }
- function Guard(firs){
-    let numbers = ("0,1,2,3,4,5,6,7,8,9");
-  let  str = firs.split('');
-   let ftr = numbers.split('');
-  return str.filter(fil => ftr.includes(fil));
 
+
+ function check(op){
+  let opeartors = ["+","-","*","/"];
+      if (!opeartors.some(op => Scre.textContent.includes(op))) {
+    console.log("nofunction")
+     operand = ""; } 
+
+    if(op === "+") {  return 1;}
+    if(op === "-"){ return 2;}
+    if(op === "*"){  return 3;}
+    if(op === "/"){ return 4;}
  }
+
 
 function OperaPress(oper){
     if(first === ""){
-         first += Scre.textContent;
+         first = Scre.textContent;
     }
-    operand += check(oper);
-   
+    operand = check(oper);
 }
 
-function Equal(first, second, operand){
+function Equal(){
+    let numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "."];
     if(second ==""){return;}
-  Guard(first);
-  Guard(second);
-  parseFloat(first);
-  parseFloat(second);
-  switch(operand){
-    case "+": Scre.textContent = ""; let result =parseFloat(first) + parseFloat(second); first = result; Scre.textContent = result; second = "";
+    second = second.split('').filter(sin => numbers.includes(sin)).join("");
+     first = first.split('').filter(sin => numbers.includes(sin)).join("");
+first =  parseFloat(first);
+second = parseFloat(second);
+console.log(typeof second);
+console.log(first);
+console.log(second);
 
-  }
+  if(operand== 1){result = first + second;}
+  if(operand == 2){result = first - second;}
+  if(operand == 3){result = first * second;}
+  if(operand == 4){ if(second === 0){Scre.textContent="ERROR"; return;} result = first / second;}
+
+  first = result.toString();
+  console.log(second);
+      second = "";
+      operand = "";
+     Scre.textContent = result;
+     
+}
+
+function clearState(){
+    first = '';
+    second = '';
+    operand = '';
+    Scre.textContent = "";
+    return;
 }
