7d1f9a9415ef0351912806835f9553392263377d
Yami4286 <abdulhanan8974@gmail.com>
2025-09-02 14:26:35 +0500

    Just put comments in the code to help understand

--- java.js
+++ java.js
@@ -8,19 +8,21 @@ const btn = document.querySelectorAll(".num");
  const maxChar = 10;
 btn.forEach(Ele => {Ele.addEventListener("click", () => { const getValue = Ele.getAttribute("value");
      if(Scre.textContent === ""){
-        clearState();
+        clearState(); 
+        //clearState if user remove all charcter just pressing c , if screen is empty, all variables gets empty too
      }
     if(getValue === "="){ 
+        //this equals the result when press "="
         const Ans = Equal();
      return;
-    }
+    }//the following getValue === "E" is CE , which clearState 
   if(getValue === "E"){clearState(); return;}
 
-
+     //this following assigned the operand value, when user press any operand
     if(getValue === "+" || getValue === "-" || getValue === "/" || getValue === "*"){OperaPress(getValue); }
     
  if(getValue === "."){
-   
+   // this conditions are for , user dont press dot more than once
      if(first === "" && Scre.textContent.includes(".")){
          
          return;
@@ -38,6 +40,7 @@ btn.forEach(Ele => {Ele.addEventListener("click", () => { const getValue = Ele.g
  
 
     if(getValue === "C"){
+        //this is to remove single character from screen
          Scre.textContent = Scre.textContent.slice(0, -1); 
     return;}
     if(first === "" || operand === ""){
@@ -52,6 +55,7 @@ btn.forEach(Ele => {Ele.addEventListener("click", () => { const getValue = Ele.g
 
     let ans = Scre.textContent;
     if(ans.length >= maxChar){
+        //this is to ensure not more than 10 characters are written on the screen
        let anse = ans.split("");
         anse.pop();
         ans = anse.join('');  
@@ -63,11 +67,12 @@ btn.forEach(Ele => {Ele.addEventListener("click", () => { const getValue = Ele.g
 
 
  function check(op){
+    //this checks for any ghost operands
   let opeartors = ["+","-","*","/"];
       if (!opeartors.some(op => Scre.textContent.includes(op))) {
     console.log("nofunction")
      operand = ""; } 
-
+ //this to see which operand is chosen by user and then return a value according to it
     if(op === "+") {  return 1;}
     if(op === "-"){ return 2;}
     if(op === "*"){  return 3;}
@@ -83,21 +88,23 @@ function OperaPress(oper){
 }
 
 function Equal(){
+    //this is a little guard which ensures that only numbers are stored in the first and second variables
     let numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "."];
     if(second ==""){return;}
     second = second.split('').filter(sin => numbers.includes(sin)).join("");
      first = first.split('').filter(sin => numbers.includes(sin)).join("");
+
 first =  parseFloat(first);
 second = parseFloat(second);
 console.log(typeof second);
 console.log(first);
 console.log(second);
-
+//this calculates the results
   if(operand== 1){result = first + second;}
   if(operand == 2){result = first - second;}
   if(operand == 3){result = first * second;}
   if(operand == 4){ if(second === 0){Scre.textContent="ERROR"; return;} result = first / second;}
-
+//this stores the result in first variable as string and reset second and operand varaible
   first = result.toString();
   console.log(second);
       second = "";
